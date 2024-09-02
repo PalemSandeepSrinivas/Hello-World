02f09e0a25473deffefdcf649d4a3c39ffc55f55
sandeep <dsnsandeepsrinivas@gmail.com>
2024-09-02 22:46:49 +0530

    Commiting multiple file together

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import keys
-from dotenv import load_dotenv
-import os
-
-def configure():
-    load_dotenv()
-
-def main():
-    configure()
-    print(keys.some_key)
-    print(os.getenv('api_key'))
-
-
-main()
+print("My name is sandeep srinivas")
