42948c74cbaf3db7a7a1bbaf65d32a74a7246661
sandeep <dsnsandeepsrinivas@gmail.com>
2025-01-25 10:45:32 +0530

    Update

---
+++ Packages/pac1.py
@@ -0,0 +1,10 @@
+
+
+class sandeep:
+
+    def sandy(self,name):
+        peeru = name
+        return f"My name is {peeru}"
+    def deepu(self,name):
+        some_name = name
+        return self.sandy(some_name)
